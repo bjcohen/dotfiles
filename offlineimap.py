16e322c3059c916f211019fcbd6a783ee5efe013
Ben Cohen <ben@cohen-family.org>
2024-08-11 20:36:40 -0400

    offlineimap

--- offlineimap.py
+++ offlineimap.py
@@ -1,7 +1,7 @@
 from subprocess import check_output
 
 def get_pass(account):
-    return check_output("bw get password {}".format(account), shell=True).splitlines()[0]
+    return check_output("NODE_OPTIONS='--no-deprecation' bw get password {}".format(account), shell=True).splitlines()[0]
 
 def get_user(account):
-    return check_output("bw get username {}".format(account), shell=True).splitlines()[0]
+    return check_output("NODE_OPTIONS='--no-deprecation' bw get username {}".format(account), shell=True).splitlines()[0]
